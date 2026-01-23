c2b2905a52d985c67d3f31f170df560a6851435d
matheo moiron <matheo.moiron@etu.unice.fr>
2026-01-23 09:24:28 +0100

    TD 2

--- demo-app/src/main/java/com/example/App.java
+++ demo-app/src/main/java/com/example/App.java
@@ -7,4 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    public static int sum(int a, int b) { return a + b; } 
 }
--- demo-app/src/test/java/com/example/AppTest.java
+++ demo-app/src/test/java/com/example/AppTest.java
@@ -3,7 +3,8 @@ package com.example;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 /**
  * Unit test for simple App.
  */
@@ -16,4 +17,12 @@ public class AppTest {
     public void shouldAnswerWithTrue() {
         assertTrue(true);
     }
+    
+    @Test
+    public void testSum() {
+      int result = App.sum(2, 3);
+      assertEquals(5, result, "2 + 3 should be 5");
+    }
+    
+    
 }
