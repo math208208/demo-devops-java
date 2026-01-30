946baec0b8ffae6fca087381fd642c5d632e9fe7
matheo moiron <matheo.moiron@etu.unice.fr>
2026-01-30 12:03:22 +0100

    TD 4

--- demo-app/src/main/java/com/example/App.java
+++ demo-app/src/main/java/com/example/App.java
@@ -1,12 +1,20 @@
 package com.example;
 
-/**
- * Hello world!
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
     }
-    
-    public static int sum(int a, int b) { return a + b; } 
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World depuis Kubernetes !";
+    }
 }
--- demo-app/src/test/java/com/example/AppTest.java
+++ demo-app/src/test/java/com/example/AppTest.java
@@ -3,26 +3,26 @@ package com.example;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-/**
- * Unit test for simple App.
- */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        String response = this.restTemplate.getForObject("http://localhost:" + port + "/", String.class);
+        
+        assertThat(response).contains("Hello World");
     }
-    
-    @Test
-    public void testSum() {
-      int result = App.sum(2, 3);
-      assertEquals(5, result, "2 + 3 should be 5");
-    }
-    
-    
 }
