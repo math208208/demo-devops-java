701111cd4756464316c9d9635b6271989711d350
matheo moiron <matheo.moiron@etu.unice.fr>
2026-02-06 09:55:42 +0100

    Debut Part 5

--- demo-app/src/main/java/com/example/App.java
+++ demo-app/src/main/java/com/example/App.java
@@ -4,19 +4,35 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 
+import io.micrometer.prometheusmetrics.PrometheusConfig;
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
+import io.prometheus.metrics.exporter.httpserver.HTTPServer;
 
 public class App {
 
-	public static void main(String[] args) throws Exception {
-		HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", 8080), 0);        
-        server.createContext("/", exchange -> {
-            String resp = "Hello World";
+    public static void main(String[] args) throws Exception {
+        PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+
+
+        HTTPServer metricServer = HTTPServer.builder()
+                .port(8081)
+                .buildAndStart();
+
+        System.out.println("📊 Métriques disponibles sur http://localhost:8081/metrics");
+
+        HttpServer appServer = HttpServer.create(new InetSocketAddress("0.0.0.0", 8080), 0);
+        
+        appServer.createContext("/", exchange -> {
+            prometheusRegistry.counter("visites_total").increment();
+            
+            String resp = "Hello World - MIAGE DevOps Demo";
             exchange.sendResponseHeaders(200, resp.length());
             try (OutputStream os = exchange.getResponseBody()) {
                 os.write(resp.getBytes());
             }
         });
 
-        server.start();
+        appServer.start();
+        System.out.println("🚀 Application lancée sur http://localhost:8080");
     }
 }
